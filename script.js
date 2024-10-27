dbe7d8a43f75054c86f7565fb2afec8b802291db
TonAlmeida <eltonsantos.almeida@outlook.com>
2024-10-27 01:29:23 -0500

    implementing previus button

--- script.js
+++ script.js
@@ -8,22 +8,33 @@ next.addEventListener(`click`, nextSlide);
 previous.addEventListener(`click`, previousSlide);
 
 function nextSlide() {
-  if (current === sliders.length) {
+  if (current === sliders.length - 1) {
     console.log(`into if condition of end`);
-    current = 0;
+    current = -1;
     sliderContainer.style.transition = `none`;
     sliderContainer.style.transform = `translateX(0)`;
     setTimeout(() => {
       sliderContainer.style.transition = `transform 0.5s ease-in-out`;
-      console.log(`time out end`);
-    }, 50);
+    }, 10);
   }
   console.log(`next`, current, sliders.length);
   current++;
   sliderContainer.style.transform = `translateX(-${current * 100}vw)`;
 }
 
-function previousSlide() {}
+function previousSlide() {
+  if (current === 0) {
+    current = sliders.length;
+    sliderContainer.style.transition = `none`;
+    sliderContainer.style.transform = `translateX(-${current * 100}vw)`;
+    setTimeout(() => {
+      sliderContainer.style.transition = `transform 0.5s ease-in-out`;
+    }, 10);
+  }
+  console.log(current, sliders.length);
+  current--;
+  sliderContainer.style.transform = `translateX(-${current * 100}vw)`;
+}
 
 // function load() {
 //   let time = 3500;
